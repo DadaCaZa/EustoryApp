ff8e5a523f0a2a72136cbfa201c5c9344432d854
DadaCaza <eustory.app.4@gmail.com>
2018-05-20 22:22:48 +0200

    Configuraciones de los tags

--- src/pages/poi-view/poi-view.ts
+++ src/pages/poi-view/poi-view.ts
@@ -21,7 +21,11 @@ export class PoiViewPage {
   images=['1.png','geolo.png','lector-qr.png','reconImag.png']
   //Nombre del poi seleccionado
   namePoi: string="";
+  //array de las foto para la linea temporal
   imgOldNew=['1.png','geolo.png'];
+  //Tags del poi
+  tags=['palacete','antiguo','tiempo','historia','Bilbao'];
+
   constructor(public navCtrl: NavController, public navParams: NavParams,public modalCtrl: ModalController) {
     this.namePoi= this.navParams.get("namePoi");
     
